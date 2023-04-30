91191a4c18f9ab6ac7cb3f50cf3df01a01e1543c
Jonathan F.da Silva <122324697+JoniSil@users.noreply.github.com>
2023-04-29 21:28:13 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+   var a = window.document.getElementById('topo')
+   a.addEventListener('mouseenter', entrar)
+   a.addEventListener('mouseout', sair)
+   function entrar(){
+    a.innerText = 'Entrou!'
+    
+}function sair(){
+    a.innerText = 'Saiu!'
+    
+}
+   
+
+
+
+
+
+/*
+var a = window.document.getElementById('icone')
+a.addEventListener('mouseenter,entrar')
+a.addEventListener('mouseout,sair')
+
+function entrar(foto){
+    a.innerText = 'Entrou!'
+    a.style.background = 'blue'
+}function sair(){
+    a.innerText = 'Saiu!'
+    a.style.background = 'green'
+}
+}*/
